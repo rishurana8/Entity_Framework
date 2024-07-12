eff1ee2904a9e8e6a56de749cae25381799dad6e
Rishu Rana <Rishu.Rana@assetmark.com>
2024-07-12 15:22:13 +0530

    Added Migrations in Database

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IntroductionToEFCoreENG.Entites;
+using Microsoft.EntityFrameworkCore;
 
 namespace IntroductionToEFCoreENG
 {
@@ -7,6 +8,16 @@ namespace IntroductionToEFCoreENG
         public ApplicationDbContext(DbContextOptions options) : base(options) { 
         
         }
+        // Table hoga Genre type ka and iska naam hai Genres, genere type jo hai woh humne class define kr rkhi hai jiske andar 2 colums honge name and id
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //modelBuilder.Entity<Genre>().HasKey(g=>g.Identifier);
+        }
+
         public DbSet<Genre> Genres { get; set; }
 
         
--- Entites/Genre.cs
+++ Entites/Genre.cs
@@ -1,9 +1,14 @@
-﻿namespace IntroductionToEFCoreENG.Entites
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroductionToEFCoreENG.Entites
 {
-    //  It is just a simple class and in order to make it entity we have to 
+    //  It is just a simple class and in order to make it entity we have to set property of dbset gener
     public class Genre
-    {
-        public int Id { get; set; }
+    { 
+        [Key]  // This will tell that it is a primary key
+        public int Identifier { get; set; }
+
+        [StringLength(maximumLength:150)]
         public string Name { get; set; }
     }
 }
