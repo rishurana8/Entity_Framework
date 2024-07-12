1fb930c7b9d273c474715ac5161ba683caace5d2
Rishu Rana <Rishu.Rana@assetmark.com>
2024-07-12 21:48:33 +0530

    doing actorcreationDTO

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroductionToEFCoreENG.Controllers
+{
+    public class ActorsController: ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ApplicationDbContext context;
+
+        public ActorsController(ApplicationDbContext context, IMapper mapper) {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+
+        }
+    }
+}
---
+++ Controllers/DTOs/ActorCreationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace IntroductionToEFCoreENG.Controllers.DTOs
+{
+    public class ActorCreationDTO
+    {
+        public class ActorCreationDTO
+        {
+            public string? Name { get; set; } = null!;
+            public decimal Fortune { get; set; }
+
+            public DateTime DateOfBirth { get; set; }
+        }
+    }
+}
